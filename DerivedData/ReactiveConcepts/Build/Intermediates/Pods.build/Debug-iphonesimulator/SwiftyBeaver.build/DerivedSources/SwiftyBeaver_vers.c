ce186244021bdb6ce50665c9934de9135d27ed94
Shahid <shahid@prolifiq.com>
2016-04-11 13:57:45 -0700

    Added the pod files

---
+++ DerivedData/ReactiveConcepts/Build/Intermediates/Pods.build/Debug-iphonesimulator/SwiftyBeaver.build/DerivedSources/SwiftyBeaver_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char SwiftyBeaverVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SwiftyBeaver  PROJECT:Pods-1" "\n"; const double SwiftyBeaverVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+use_frameworks!
+
+
+pod 'ReactiveCocoa', '~> 4.0.0'
+pod 'SwiftyBeaver', '~> 0.3.0'
+pod 'PureLayout', '~> 3.0.0'
+
